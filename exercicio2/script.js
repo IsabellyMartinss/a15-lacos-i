c25f7880aea23f15b4fd0e601ca083a3ecb771e4
Isabelly Martins <isabellyksml@gmail.com>
2022-08-16 00:53:53 -0300

    Exercícios de fixação - Aula 015 - Concluídos

---
+++ exercicio2/script.js
@@ -0,0 +1,4 @@
+for(let i = 1; i<=10; i++){
+console.log(i*22)
+}
+console.log('Tabuada de 22 ok')
---
+++ exercicio3/script.js
@@ -0,0 +1,4 @@
+let numero = Number(prompt("Veja aqui a tabuada de qualquer número, digite o número desejado"))
+for(let i = 0; i<=10; i++){
+    console.log(i*numero)
+}
